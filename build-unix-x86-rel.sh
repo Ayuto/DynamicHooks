845c64b4ea2b9634a2d7a21403565dd86b529343
Ayuto <ayuto@web.de>
2015-04-16 14:51:35 +0200

    Updated linux build script

--- build-unix-x86-rel.sh
+++ build-unix-x86-rel.sh
@@ -1,11 +1,12 @@
-mkdir Build
-mkdir Build/unix-x86
+set -e
+
+mkdir -p Build/unix-x86
 cd Build/unix-x86
 cmake ../../src -G"Unix Makefiles"
 make
 cd ../..
 
-mkdir Build/unix-x86-tests
+mkdir -p Build/unix-x86-tests
 cd Build/unix-x86-tests
 cmake ../../tests -G"Unix Makefiles"
 make
