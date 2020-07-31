26a6f827b21656e1f3d4b3d9e04727420f1e13c3
Jonathan <30329245+CookStar@users.noreply.github.com>
2020-07-31 11:38:42 +0900

    Converted run_tests.py to Python3.

--- build-windows-vs2010-x86-rel.bat
+++ build-windows-vs2010-x86-rel.bat
@@ -10,7 +10,7 @@ cmake ..\..\tests -G"Visual Studio 10" || goto :error
 msbuild Tests.sln /p:Configuration=Release /p:VCTargetsPath="C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0" || goto :error
 cd ..\..
 
-python run_tests.py Build/vs2010-x86-tests/Release || goto :error
+python3 run_tests.py Build/vs2010-x86-tests/Release || goto :error
 goto :EOF
 
 :error
