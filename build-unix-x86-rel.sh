92f9de59c72be4bf4f7ba5d74e987ab12a031fd4
Ayuto <ayuto@web.de>
2025-04-17 20:41:13 +0200

    Add static AsmJit lib for Linux

--- build-unix-x86-rel.sh
+++ build-unix-x86-rel.sh
@@ -2,14 +2,14 @@ set -e
 
 mkdir -p Build/unix-x86
 cd Build/unix-x86
-cmake ../../src -G"Unix Makefiles"
+cmake ../../src -G"Unix Makefiles" -DASMJIT_STATIC=1
 make
 cd ../..
 
 mkdir -p Build/unix-x86-tests
 cd Build/unix-x86-tests
-cmake ../../tests -G"Unix Makefiles"
+cmake ../../tests -G"Unix Makefiles" -DASMJIT_STATIC=1
 make
 cd ../..
 
-python run_tests.py Build/unix-x86-tests
+python3 run_tests.py Build/unix-x86-tests
